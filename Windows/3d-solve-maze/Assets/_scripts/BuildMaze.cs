261c02a0b3732f4bedbd6de555fb250808ebb7b4
Tyler Hostler-Mathis <Hostler.Mathis.Tyler@gmail.com>
2018-04-30 13:08:25 -0400

    3d-maze-first

---
+++ Windows/3d-solve-maze/Assets/_scripts/BuildMaze.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildMaze : MonoBehaviour {
+
+	public GameObject floor;
+
+	public GameObject rightHand;
+	public GameObject leftHand;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
